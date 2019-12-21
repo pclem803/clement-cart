dc3005b3a951f4e3783cbb0ddb9772d48eeeee8c
Philip Clement <pclement803@gmail.com>
2019-12-21 11:57:14 -0600

    initial add to cart state functional

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import {
   Button,
   Container,
   Title,
-  Message,
   Notification,
   Content,
-  Modal
+  Modal,
+  Card,
+  Heading
 } from "rbx";
 import "rbx/index.css";
 
 const App = () => {
   const [data, setData] = useState({});
-  const [cart, setCart] = useState({});
+  const [cart, setCart] = useState([]);
   const [open, openCart] = useState(false);
 
   const products = Object.values(data);
@@ -31,60 +32,100 @@ const App = () => {
   return (
     <Container>
       <br></br>
-      <Banner cartState = {{open, openCart}}/>
-      <Inventory products={products} />
-      <ShowCart cartState = {{open, openCart}}/>
+      <Banner cartState={{ open, openCart }} />
+      <Inventory products={products} addState={{ cart, setCart }} />
+      <ShowCart cartState={{ open, openCart }} addState={{ cart, setCart }} />
     </Container>
   );
 };
 
-const ShowCart = ({cartState}) => {
-  if (cartState.open==true){
-    return(<Modal active>
-      <Modal.Background onClick = {() => (
-        cartState.openCart(false)
-      )}/>
-      <Modal.Content>
-        <LoadCart/>
-      </Modal.Content>
-      <Modal.Close onClick = {() => (
-        cartState.openCart(false)
-      )}/>
-    </Modal>)
+const ShowCart = ({ cartState, addState }) => {
+  if (cartState.open === true) {
+    return (
+      <Modal active>
+        <Modal.Background onClick={() => cartState.openCart(false)} />
+        <Modal.Content>
+          <LoadCart items={addState} />
+        </Modal.Content>
+        <Modal.Close onClick={() => cartState.openCart(false)} />
+      </Modal>
+    );
+  } else {
+    return <Modal></Modal>;
   }
-  else {
-    return(<Modal>
-      <Modal.Background />
-      <Modal.Content>
-        This your cart!
-      </Modal.Content>
-      <Modal.Close/>
-    </Modal>)
+};
+
+const LoadCart = ({ items }) => {
+  let my_items = items.cart;
+  if (my_items.length===0){
+    return(
+      <Content>
+        <Notification color="primary" textAlign="centered">
+          Your Cart
+        </Notification>
+        <Notification color="dark" textAlign="centered">
+          Add items to your cart to get started!
+        </Notification>
+
+      </Content>
+    )
   }
-}
+  return (
+    <Card>
+      <Box>
+        <Content>
+          <Notification color="primary" textAlign="centered">
+            <Title>Your Cart</Title>
+            {my_items.map(item => (
+              <CartProduct item={item} />
+            ))}
+          </Notification>
+          <Button size="large" color="dark" fullwidth>
+            Checkout
+          </Button>
+        </Content>
+      </Box>
+    </Card>
+  );
+};
 
-const LoadCart = () =>{
-  return(
-    <Box>
-      This is the cart
-    </Box>
-  )
-}
+const CartProduct = ({ item }) => {
+  return (
+    <Notification>
+        <Column size={4}>
+        <Image.Container size={64}>
+          <GetImage code={item.product.sku}></GetImage>
+        </Image.Container>
+        </Column>
+        <Column size={8}>
+        <Content>
+          <Heading>
+            {item.product.title}
+          </Heading>
+        </Content>
+        </Column>
+    </Notification>
+  );
+};
 
-const Banner = ({cartState}) => (
+const Banner = ({ cartState }) => (
   <React.Fragment>
     <Notification textAlign="centered" color="primary">
       <Title>Welcome to Clement Cart!</Title>
     </Notification>
     <Button.Group align="centered">
-      <Button size="large"
-      color="dark"
-      onClick = {()=>cartState.openCart(true)}>View Cart</Button>
+      <Button
+        size="large"
+        color="dark"
+        onClick={() => cartState.openCart(true)}
+      >
+        View Cart
+      </Button>
     </Button.Group>
   </React.Fragment>
 );
 
-const Inventory = ({ products }) => {
+const Inventory = ({ products, addState }) => {
   return (
     <React.Fragment>
       <Column.Group breakpoint="desktop" multiline gapSize={6}>
@@ -93,7 +134,7 @@ const Inventory = ({ products }) => {
             <Box color="primary" textAlign="center" size={20}>
               <Content>{this_product.title}</Content>
               <GetImage code={this_product.sku} />
-              <MakeDesc code={this_product} />
+              <MakeDesc code={this_product} addState={addState} />
             </Box>
           </Column>
         ))}
@@ -102,7 +143,7 @@ const Inventory = ({ products }) => {
   );
 };
 
-const MakeDesc = ({ code }) => {
+const MakeDesc = ({ code, addState }) => {
   let code_string = code.price.toString();
   if (!code_string.includes(".")) {
     code_string += ".";
@@ -117,18 +158,50 @@ const MakeDesc = ({ code }) => {
     <Content size="large">
       $<strong>{dollar_price}</strong>
       {cents}
-      <MakeSize />
+      <MakeSize product={code} state={addState} />
     </Content>
   );
 };
 
-const MakeSize = () => {
+const MakeSize = ({ product, state }) => {
   return (
     <Button.Group size="medium" hasAddons align="centered">
-      <Button>S</Button>
-      <Button>M</Button>
-      <Button>L</Button>
-      <Button>XL</Button>
+      <Button
+        onClick={() => {
+          let array = state.cart;
+          array.push({ product });
+          state.setCart(array);
+        }}
+      >
+        S
+      </Button>
+      <Button
+        onClick={() => {
+          let array = state.cart;
+          array.push({ product });
+          state.setCart(array);
+        }}
+      >
+        M
+      </Button>
+      <Button
+        onClick={() => {
+          let array = state.cart;
+          array.push({ product });
+          state.setCart(array);
+        }}
+      >
+        L
+      </Button>
+      <Button
+        onClick={() => {
+          let array = state.cart;
+          array.push({ product });
+          state.setCart(array);
+        }}
+      >
+        XL
+      </Button>
     </Button.Group>
   );
 };
@@ -136,11 +209,7 @@ const MakeSize = () => {
 const GetImage = ({ code }) => {
   const url = "/data/products/" + code + "_1.jpg";
   console.log(url);
-  return (
-    <Image.Container>
-      <Image src={url} />
-    </Image.Container>
-  );
+  return <Image src={url} />;
 };
 
 export default App;
